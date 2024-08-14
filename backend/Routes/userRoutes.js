ee77e30d3851f8ae23ac84d716efe77d5d270d74
rohan <rohanjaiswal511@gmail.com>
2024-08-14 19:24:03 +0530

    Add Profile update

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser, authUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  authUser,
+  updateUserProfile,
+} = require("../Controllers/userController");
+const { protect } = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
+router.route("/profile").post(protect, updateUserProfile);
 
 module.exports = router;
